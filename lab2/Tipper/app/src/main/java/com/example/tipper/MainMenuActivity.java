350164102c89d3d8a52956969986b5194f5a8b8c
Sebastian Lewandowski <hulio90@gmail.com>
2022-03-31 23:57:12 +0200

    added logo and button for BMI Calculator in main activity

--- lab2/Tipper/app/src/main/java/com/example/tipper/MainMenuActivity.java
+++ lab2/Tipper/app/src/main/java/com/example/tipper/MainMenuActivity.java
@@ -2,13 +2,32 @@ package com.example.tipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    Button btnOpenCalculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        btnOpenCalculator = (Button)findViewById(R.id.btnOpenCalculator);
+
+        btnOpenCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCalculatorActivityOnButtonClick();
+            }
+        });
     }
+    private void openCalculatorActivityOnButtonClick() {
+        Intent calculatorActivity = new Intent(this, BMICalculatorActivity.class);
+        startActivity(calculatorActivity);
+    }
+
 }
