3fd597a9f65ab787f8dcc125dfe5762a864fa743
Sebastian Lewandowski <hulio90@gmail.com>
2022-04-01 14:10:15 +0200

    added kcal activity

--- lab2/Tipper/app/src/main/java/com/example/tipper/MainMenuActivity.java
+++ lab2/Tipper/app/src/main/java/com/example/tipper/MainMenuActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainMenuActivity extends AppCompatActivity {
 
     Button btnOpenCalculator;
+    Button btnOpenKcalCalculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +18,35 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         btnOpenCalculator = (Button)findViewById(R.id.btnOpenCalculator);
+        btnOpenKcalCalculator = (Button)findViewById(R.id.btnOpenKcalCalculator);
 
+        //BMI Calculator
         btnOpenCalculator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openCalculatorActivityOnButtonClick();
             }
         });
+
+        //Kcal Calculator
+        btnOpenKcalCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openKcalCalculatorActivityOnButtonClick();
+            }
+        });
     }
+
+    //BMI method
     private void openCalculatorActivityOnButtonClick() {
         Intent calculatorActivity = new Intent(this, BMICalculatorActivity.class);
         startActivity(calculatorActivity);
+    }
+
+    //kcal method
+    private void openKcalCalculatorActivityOnButtonClick() {
+        Intent kcalCalculatorActivity = new Intent(this, KCalsCalculatorActivity.class);
+        startActivity(kcalCalculatorActivity);
     }
 
 }
