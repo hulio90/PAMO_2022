d0c3b8e14a2f1c92e4569816f6bfb9b264e9e890
Sebastian Lewandowski <hulio90@gmail.com>
2022-04-03 00:22:49 +0200

    added recipes activity

--- lab2/Tipper/app/src/main/java/com/example/tipper/KCalsCalculatorActivity.java
+++ lab2/Tipper/app/src/main/java/com/example/tipper/KCalsCalculatorActivity.java
@@ -2,12 +2,14 @@ package com.example.tipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -29,6 +31,7 @@ public class KCalsCalculatorActivity extends AppCompatActivity implements Adapte
     private double weight = 0.0;
     private int height = 0;
     private double KcalValue = 0.0;
+    private Button btnRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,7 @@ public class KCalsCalculatorActivity extends AppCompatActivity implements Adapte
 
         KcalTextView = (TextView ) findViewById(R.id.KcalTextView);
         spinner = (Spinner) findViewById(R.id.sex_spinner);
+        btnRecipes = (Button) findViewById(R.id.btnRecipes);
 
         KcalTextView.setText(numberFormat.format(0));
 
@@ -50,26 +54,51 @@ public class KCalsCalculatorActivity extends AppCompatActivity implements Adapte
         ageEditText = (EditText) findViewById(R.id.input_your_age);
         weightEditText = (EditText) findViewById(R.id.input_your_weight);
         heightEditText = (EditText) findViewById(R.id.input_your_height);
+        ageEditText.addTextChangedListener(ageEditTextWatcher);
+        weightEditText.addTextChangedListener(weightEditTextWatcher);
         heightEditText.addTextChangedListener(heightEditTextWatcher);
+
+        btnRecipes.setVisibility(View.INVISIBLE);
+
+        //button Recipes
+        btnRecipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRecipesActivityOnButtonClick();
+            }
+        });
     }
 
     private void calculate() {
-        age = Integer.parseInt(ageEditText.getText().toString());
-        weight = Double.parseDouble(weightEditText.getText().toString());
-        height = Integer.parseInt(heightEditText.getText().toString());
+
+        try {
+            age = Integer.parseInt(ageEditText.getText().toString());
+            weight = Double.parseDouble(weightEditText.getText().toString());
+            height = Integer.parseInt(heightEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            age = 0;
+            weight = 0.00;
+            height = 0;
+        }
 
         //male
-        KcalValue = 66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age);
+        if (str.equals("Male"))
+            KcalValue = 66.5 + (13.75 * weight) + (5.003 * height) - (6.775 * age);
 
         //female
-        //KcalValue = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
+        if (str.equals("Female"))
+            KcalValue = 655.1 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
 
         KcalTextView.setText(numberFormat.format(KcalValue));
+
+        if (age > 1 && weight > 1 && height > 1)
+            btnRecipes.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         str = spinner.getSelectedItem().toString();
+        calculate();
     }
 
     @Override
@@ -77,8 +106,25 @@ public class KCalsCalculatorActivity extends AppCompatActivity implements Adapte
 
     }
 
-    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
+    private final TextWatcher ageEditTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence charSequence, int start,
+                                  int before, int count) {
+            calculate();
+        }
 
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int start,
+                                      int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+    private final TextWatcher weightEditTextWatcher = new TextWatcher() {
         @Override
         public void onTextChanged(CharSequence charSequence, int start,
                                   int before, int count) {
@@ -94,5 +140,28 @@ public class KCalsCalculatorActivity extends AppCompatActivity implements Adapte
         public void afterTextChanged(Editable editable) {
         }
     };
+
+    private final TextWatcher heightEditTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence charSequence, int start,
+                                  int before, int count) {
+            calculate();
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int start,
+                                      int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+        }
+    };
+
+    //button method
+    private void openRecipesActivityOnButtonClick() {
+        Intent recipeActivity = new Intent(this, RecipesActivity.class);
+        startActivity(recipeActivity);
+    }
 
 }
