c0f3a86ef7e14b11109cee95199663bf6e09ded2
Sebastian Lewandowski <hulio90@gmail.com>
2022-04-18 13:47:36 +0200

    added basic version of BMI chart

--- lab3/Tipper/app/src/main/java/com/example/tipper/ChartActivity.java
+++ lab3/Tipper/app/src/main/java/com/example/tipper/ChartActivity.java
@@ -5,8 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -18,6 +24,7 @@ public class ChartActivity extends AppCompatActivity {
 
     String stringBMIValue;
     double doubleBMIValue;
+    BarChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +33,12 @@ public class ChartActivity extends AppCompatActivity {
         String bmiValueToString = getIntent().getStringExtra("bmiValueToString");
         doubleBMIValue = Double.parseDouble(bmiValueToString);
 
+        chart = (BarChart) findViewById(R.id.chart);
+
         bmiFormatter();
         getTodayDate();
+
+        exampleChart();
     }
 
     private void bmiFormatter() {
@@ -39,4 +50,24 @@ public class ChartActivity extends AppCompatActivity {
         String currentDate = sdf.format(new Date());
         System.out.println(currentDate);
     }
+
+    public void exampleChart() {
+        ArrayList<BarEntry> bmi = new ArrayList<BarEntry>();
+
+        float getBMI = Float.parseFloat(stringBMIValue);
+
+        bmi.add(new BarEntry(1,35.43f));
+        bmi.add(new BarEntry(2,35.73f));
+        bmi.add(new BarEntry(3,35.55f));
+        bmi.add(new BarEntry(4,35.21f));
+        bmi.add(new BarEntry(5,35.28f));
+        bmi.add(new BarEntry(6,35.24f));
+        bmi.add(new BarEntry(7,getBMI));
+
+        BarDataSet barDataSet = new BarDataSet(bmi, "bmi");
+        barDataSet.setValueTextSize(16f);
+        BarData barData = new BarData(barDataSet);
+        chart.setData(barData);
+    }
+
 }
