f04e15eb68db86ea3a47838fa34cf5d5ef555945
Sebastian Lewandowski <hulio90@gmail.com>
2022-04-18 14:52:03 +0200

    added new chart which print bmi value and day+month

--- lab3/Tipper/app/src/main/java/com/example/tipper/ChartActivity.java
+++ lab3/Tipper/app/src/main/java/com/example/tipper/ChartActivity.java
@@ -3,12 +3,16 @@ package com.example.tipper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -20,11 +24,12 @@ public class ChartActivity extends AppCompatActivity {
 
     private static final DecimalFormat df = new DecimalFormat("0.00");
     @SuppressLint("SimpleDateFormat")
-    SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+    SimpleDateFormat sdf = new SimpleDateFormat("dd.MM");
 
     String stringBMIValue;
     double doubleBMIValue;
     BarChart chart;
+    String currentDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +43,11 @@ public class ChartActivity extends AppCompatActivity {
         bmiFormatter();
         getTodayDate();
 
-        exampleChart();
+        //first version of chart
+        //exampleChart();
+
+        //second version of chart(printing date + month as x value)
+        showBMIBars();
     }
 
     private void bmiFormatter() {
@@ -47,7 +56,7 @@ public class ChartActivity extends AppCompatActivity {
     }
 
     private void getTodayDate() {
-        String currentDate = sdf.format(new Date());
+        currentDate = sdf.format(new Date());
         System.out.println(currentDate);
     }
 
@@ -68,6 +77,61 @@ public class ChartActivity extends AppCompatActivity {
         barDataSet.setValueTextSize(16f);
         BarData barData = new BarData(barDataSet);
         chart.setData(barData);
+    }
+
+    private void showBMIBars(){
+        double getBMIDouble = Double.parseDouble(stringBMIValue);
+
+        ArrayList<Double> bmiValueList = new ArrayList<Double>();
+        bmiValueList.add((double)35.65);
+        bmiValueList.add((double)35.52);
+        bmiValueList.add((double)35.44);
+        bmiValueList.add((double)getBMIDouble);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        for (int i = 0; i < bmiValueList.size(); i++) {
+            BarEntry barEntry = new BarEntry(i+1, bmiValueList.get(i).floatValue());
+            entries.add(barEntry);
+        }
+        final ArrayList<String> xAxisLabel = new ArrayList<>();
+        xAxisLabel.add("22.02");
+        xAxisLabel.add("02.03");
+        xAxisLabel.add("12.03");
+        xAxisLabel.add(currentDate);
+        xAxisLabel.add("");
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setTextSize(12);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setAxisLineColor(Color.BLACK);
+        xAxis.setAxisMinimum(0 + 0.5f);
+        xAxis.setAxisMaximum(entries.size() + 0.5f);
+        xAxis.setLabelCount(xAxisLabel.size(), true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return xAxisLabel.get((int) value);
+            }
+        });
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        rightAxis.setTextSize(12);
+        rightAxis.setTextColor(Color.BLACK);
+        rightAxis.setAxisLineColor(Color.BLACK);
+        rightAxis.setLabelCount(4, true);
+
+        BarDataSet barDataSet = new BarDataSet(entries, "BMI");
+        barDataSet.setColor(Color.rgb(0, 0, 255));
+        barDataSet.setFormSize(14f);
+        barDataSet.setValueTextSize(12f);
+        BarData data = new BarData(barDataSet);
+        chart.setData(data);
+        chart.getLegend().setEnabled(false);
+        chart.getDescription().setEnabled(false);
     }
 
 }
